115b32a15848eb3e967c46c5b89adb1ab8c7eb40
Rodriasd <veronrodrigo98@gmail.com>
2024-04-30 16:50:01 -0300

    was added the fi for animes

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 import csv
 import pandas as pd
+from collections import Counter
 
 with open('archive/muestra.csv', 'r', encoding='utf-8') as file:
     lector = csv.reader(file, delimiter=',', quotechar='"')
@@ -8,6 +9,24 @@ with open('archive/muestra.csv', 'r', encoding='utf-8') as file:
         data.append(row)
     
 
-a = pd.Series(data)
+fr=[]
+animes=[]
+
 for i in range(len(data)):
-    print(data[i][5])
+    animes.append(data[i][5])
+    # if data[i][5] not in animes:
+    #     animes.append(data[i][5])
+
+def obtener_fi(lista_de_datos):
+    lista_de_datos.sort()
+    fi = {}
+    for i in lista_de_datos:
+        if i in fi:
+            fi[i] += 1
+        else:
+            fi[i] = 1
+    return fi
+
+tabla = obtener_fi(animes)
+
+print(tabla)
