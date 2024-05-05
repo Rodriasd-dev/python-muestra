46e3019a5b0b0f63a12551ca86053ebdbbd00a2f
Rodriasd <veronrodrigo98@gmail.com>
2024-05-05 14:53:36 -0300

    movies with a rating of 8.5 or above are saved

--- app.py
+++ app.py
@@ -3,6 +3,15 @@ import pandas as pd
 
 df = pd.read_csv("archive/anime.csv")
 
-# print(df)
 
-ratings = df["rating"]
+peliculas_mejor_rating = df[(df["rating"] > 8.5) & (df["type"] == "Movie")]
+
+
+datos_json = peliculas_mejor_rating.to_dict(orient="records")
+
+
+datos_py = "datos_json = " + repr(datos_json)
+
+
+with open("peliculas_mejor_rating.py", "w") as f:
+    f.write(datos_py)
